01884b57350e1e6060f896e61c375957fcd0515e
Roman Bylbas <romka.bulbas@gmail.com>
2017-01-25 22:11:30 +0200

    Implemented simple test

---
+++ test/index.js
@@ -0,0 +1,37 @@
+var assert  = require('assert');
+var mocha   = require('mocha');
+var winston = require('winston');
+var MySQL   = require('../lib');
+
+var config = {
+  user      : 'root',
+  password  : 'toor',
+  db        : 'database_development'
+};
+
+describe('Test winston MYSQL transport.', function() {
+  this.MySQLTransport = null;
+
+  it('Throw error if config is empty', () => {
+    assert.throws(() => {
+      new winston.transports.MySQL();
+    }, Error);
+  });
+
+  it('Does not throw error if config is correct', () => {
+    assert.doesNotThrow(() => {
+      this.MySQLTransport = new winston.transports.MySQL(config);
+      this.connection = this.MySQLTransport.connection;
+    });
+  });
+
+  it('Connection should be open', () => {
+      assert.equal(this.connection.state, 'authenticated');
+  });
+
+  it('Should write log to database', () => {
+    assert.doesNotThrow(() => {
+      this.MySQLTransport.log('info', 'testing message');
+    });
+  });
+});
